ad44531ee29d5a41a46a91064bd198fdb840cb21
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-18 00:25:03 +0530

    Hero section features container

---
+++ src/Pages/Home/FeaturesContainer.jsx
@@ -0,0 +1,18 @@
+import style from "./FeaturesContainer.module.css";
+
+function FeaturesContainer() {
+  return (
+    <section className={style.featureContainer}>
+      <div className="featureName">Logo Practical Slips</div>
+      <div className="featureDescription">
+        here we provide fy, sy and ty bbaca slip solution with try yourself
+        features
+      </div>
+      <div className="featureLink">
+        <a href="">Link</a>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesContainer;
