1368efb657cfcecaa7bb8518042ec2beb1cebdb4
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-18 00:24:48 +0530

    Home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from "../../Components/Navbar";
+import FeaturesContainer from "./FeaturesContainer";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="heroContainer">
+        <h1>what we provide you in this platform happy to help you</h1>
+        <div className="containers">
+          <FeaturesContainer />
+          <FeaturesContainer />
+          <FeaturesContainer />
+          <FeaturesContainer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
