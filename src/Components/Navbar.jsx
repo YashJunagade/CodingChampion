516ce4dbefdf82edbac9a81e076d24aedb992d87
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-18 00:24:34 +0530

    navbar with its module css file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,29 +3,30 @@ import style from "./Navbar.module.css";
 function Navbar() {
   return (
     <nav className={style.navbar}>
-      <ul>
-        <h3>Unknown</h3>
-        <div className={style.list}>
-          <li>
-            <a href="">unknow</a>
-          </li>
-          <li>
-            <a href="">unknow</a>
-          </li>
-          <li>
-            <a href="">unknow</a>
-          </li>
-          <li>
-            <a href="">unknow</a>
-          </li>
-          <li>
-            <a href="">unknow</a>
-          </li>
-          <li>
-            <a href="">unknow</a>
-          </li>
-        </div>
-      </ul>
+      <h3>Unknown</h3>
+      <div className={style.list}>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Practical Slips</a>
+        </li>
+        <li>
+          <a href="">LabBook</a>
+        </li>
+        <li>
+          <a href="">Roadmaps</a>
+        </li>
+        <li>
+          <a href="">DSA</a>
+        </li>
+      </div>
+      <div className={style.profilLogInDetails}>
+        <li>
+          <a href="">Login</a>
+        </li>
+        <div className={style.profileAvatar}></div>
+      </div>
     </nav>
   );
 }
