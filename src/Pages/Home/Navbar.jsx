ee90c809ae912f49067776b1e1592a6a7054921c
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-20 12:53:02 +0530

    navbar home page

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,34 @@
+import style from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={style.navbar}>
+      <h3>Unknown</h3>
+      <div className={style.list}>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="/slip">Practical Slips</a>
+        </li>
+        <li>
+          <a href="/labbook">LabBook</a>
+        </li>
+        <li>
+          <a href="">Roadmaps</a>
+        </li>
+        <li>
+          <a href="">DSA</a>
+        </li>
+      </div>
+      <div className={style.profilLogInDetails}>
+        <li>
+          <a href="">Login</a>
+        </li>
+        <div className={style.profileAvatar}></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
