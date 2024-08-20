70e479b1ac1942f8f7e04060441afc762b343365
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-20 12:53:26 +0530

    Slip solution page

---
+++ src/Pages/Slip/Slip.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../../Components/Navbar/Navbar";
+import SideBar from "../../Components/SideBar/SideBar";
+import Menu from "./Menu";
+import "./Slip.css";
+
+function Slip() {
+  return (
+    <section className="slipPageSection">
+      <Navbar></Navbar>
+      <div className="slipMainContainer">
+        <SideBar></SideBar>
+        <Menu></Menu>
+      </div>
+    </section>
+  );
+}
+
+export default Slip;
