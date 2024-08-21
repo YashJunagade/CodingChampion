61ce0cdf8b0410b1211f8bea98f7a6f5dc353232
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-21 08:16:21 +0530

    add some slip data

---
+++ Solutions/Slips Solutions/SlipData.jsx
@@ -0,0 +1,636 @@
+const SlipData = [
+  {
+    subject: "C Programming",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "DBMS",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Web Techonology",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "RDBMS",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Data Structure",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a menu driven program using ‘C’ for Binary Search Tree. The menu includes:\n- Create a Binary Search Tree\n- Insert element in a Binary Search Tree\n- Display",
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: "Write a ‘C’ program to evaluate a given polynomial using function. (Use array).",
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a 'C' program to accept a string from a user and reverse it using Static
+            implementation of Stack.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a 'C' program to create a Circularly Doubly Linked list and display it.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 3,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a program to create two singly linked lists of elements of type integer and
+            find the union of the linked lists. (Accept elements in the sorted order)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a 'C' program to read the adjacency matrix of a directed graph and convert
+            it into an adjacency list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 4,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a menu driven program using ‘C’ for Binary Search Tree. The menu includes:\n- Create a Binary Search Tree\n- Traverse it by using Inorder and Postorder traversing technique",
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: "Write a ‘C’ program to accept two polynomials and find the addition of accepted polynomials. (use array)",
+            marks: 10,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    subject: "Big Data",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Php",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Angular JS",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "CPP",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Advance Php",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Node JS",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Core Java",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    subject: "Python",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "MongoDB",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Advance Java",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Android Programming",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Dot Net Framework",
+    slips: [
+      {
+        slipId: 1,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to check whether a given number is Armstrong or not. (Use static keyword)",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Define an abstract class Shape...",
+            marks: 25,
+          },
+        ],
+      },
+      {
+        slipId: 2,
+        questions: [
+          {
+            questionId: 1,
+            text: "Write a C program to display the transpose of a given matrix",
+            marks: 15,
+          },
+          {
+            questionId: 2,
+            text: "Write a C program to accept a number from the user...",
+            marks: 25,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default SlipData;
+
+/* 0-c, 1-dbms, 2-web tech, 3-rdbms 4-data structure, 5-bigdata, 6-php, 7-angular js, 8-cpp, 9-advance php, 10-nodejs, 11-corejava, 12-python, 13-mongodb, 14-advance java, 15-android programming, 16-dotnot framework   */
