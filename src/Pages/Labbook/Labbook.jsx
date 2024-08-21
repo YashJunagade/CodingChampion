d263072fd1ed57edcb9246d5e4ecbf608c74c3bc
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-21 08:15:56 +0530

    Labbook page

---
+++ src/Pages/Labbook/Labbook.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../../Components/Navbar/Navbar";
+import SideBar from "../../Components/SideBar/SideBar";
+import Menu from "./Menu";
+import "./Labbook.css";
+
+function Labbook() {
+  return (
+    <section className="labbookPageSection">
+      <Navbar />
+      <div className="labbookMainContainer">
+        <SideBar />
+        <Menu />
+      </div>
+    </section>
+  );
+}
+
+export default Labbook;
