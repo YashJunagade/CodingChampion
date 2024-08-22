3b1ad39df792a71699abc4c3629cb35374d221a1
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-22 18:29:41 +0530

    added css to the code editor pannel

--- src/Pages/CodeEditor/CodeEditor.jsx
+++ src/Pages/CodeEditor/CodeEditor.jsx
@@ -1,21 +1,27 @@
 import SplitPane from "react-split-pane";
 import "./CodeEditor.css";
+import Navbar from "../Home/Navbar";
 
 function CodeEditor() {
   return (
-    <SplitPane
-      split="vertical"
-      resizerClassName="resizer"
-      defaultSize={parseInt(localStorage.getItem("splitPos"), 10)}
-      onChange={(size) => localStorage.setItem("splitPos", size)}
-      minSize={50}
-    >
-      <div className="panel panel1">this is elon musk</div>
-      <div className="panel panel2">
-        <h2>Panel 2</h2>
-        <p>Content for panel 2</p>
-      </div>
-    </SplitPane>
+    <div className="codeEditor">
+      <Navbar />
+      <SplitPane
+        className="splitContainer"
+        split="vertical"
+        resizerClassName="resizer"
+        defaultSize="40%" // Set the default size of the first panel to 40%
+        minSize={50}
+        onChange={(size) => localStorage.setItem("splitPos", size)}
+      >
+        <div className="panel panel1">this is elon musk</div>
+        <div className="panel panel2">
+          <h2>Panel 2</h2>
+          <p>Content for panel 2</p>
+        </div>
+      </SplitPane>
+    </div>
   );
 }
+
 export default CodeEditor;
