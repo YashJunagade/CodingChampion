4da75be123ccc3d9c35c86ac9c0a6f07b649dbe5
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-23 20:27:31 +0530

    created context api to get login details

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import App from "./App.jsx";
+import { AuthProvider } from "./store/UserLogIn.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
