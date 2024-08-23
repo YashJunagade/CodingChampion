66dff8822ba9ea358691c59c5c9ff0c15d4aa69e
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-24 01:15:09 +0530

    code editor with slip language and slip solution

--- src/Pages/SolutionPage/CodeEditor.jsx
+++ src/Pages/SolutionPage/CodeEditor.jsx
@@ -1,17 +1,13 @@
 import Editor from "@monaco-editor/react";
 
-function CodeEditor() {
-  const code = "console.log('Monaco Editor!');";
-
-  const language = "javascript";
-
+function CodeEditor({ language, solution }) {
   return (
     <Editor
       height="100%"
       width="100%"
       language={language}
       theme="vs-dark"
-      value={code}
+      value={solution}
     />
   );
 }
