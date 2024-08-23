1c4e9eea6d55861eec9e22dd7547efa2444fdd10
Devotion <letsmaildipak@gmail.com>
2024-08-23 19:33:17 +0530

    basic navbar responsive design

--- src/Pages/Home/FeaturesContainer.jsx
+++ src/Pages/Home/FeaturesContainer.jsx
@@ -1,6 +1,6 @@
 function FeaturesContainer() {
   return (
-    <section className="w-80 h-40 bg-primary2 p-6 mt-8 m-auto rounded shadow-md">
+    <section className="w-80 h-40 bg-primary p-6 mt-8 m-auto rounded shadow-md">
       <div className="featureName">Logo Practical Slips</div>
       <div className="featureDescription">
         here we provide fy, sy and ty bbaca slip solution with try yourself
