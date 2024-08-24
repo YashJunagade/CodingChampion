57493ca485c2fc4c642e67bf613f6a61837b36f4
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-25 01:02:29 +0530

    add home navigation

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="">
         <ul className="md:flex hidden font-semibold justify-center">
           <li className="mx-[10px] cursor-pointer hover:bg-primary2 px-2 py-2 rounded hover:ease-in-out duration-200">
-            <a href="" className="px-2 py-2">
+            <a href="/" className="px-2 py-2">
               Home
             </a>
           </li>
@@ -22,12 +22,12 @@ function Navbar() {
             </a>
           </li>
           <li className="mx-[10px] cursor-pointer hover:bg-primary2 px-2 py-2 rounded hover:ease-in-out duration-200">
-            <a href="" className="px-2 py-2">
+            <a href="/roadmaps" className="px-2 py-2">
               Roadmaps
             </a>
           </li>
           <li className="mx-[10px] cursor-pointer hover:bg-primary2 px-2 py-2 rounded hover:ease-in-out duration-200">
-            <a href="" className="px-2 py-2">
+            <a href="/dsa" className="px-2 py-2">
               DSA
             </a>
           </li>
