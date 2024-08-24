7c51c0fe283c8171a6e76a64694e69479518ecb1
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-24 11:07:15 +0530

    added datastrusture slip questions

--- Solutions/Slips Solutions/SlipData.jsx
+++ Solutions/Slips Solutions/SlipData.jsx
@@ -175,6 +175,7 @@ int main()
     ],
     language: "plsql",
   },
+
   {
     subject: "Data Structure",
     slips: [
@@ -183,12 +184,12 @@ int main()
         questions: [
           {
             questionId: 1,
-            text: "Write a menu driven program using ‘C’ for Binary Search Tree. The menu includes:\n- Create a Binary Search Tree\n- Insert element in a Binary Search Tree\n- Display",
+            text: `Write a menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Insert element in a Binary Search Tree<br> - Display`,
             marks: 20,
           },
           {
             questionId: 2,
-            text: "Write a ‘C’ program to evaluate a given polynomial using function. (Use array).",
+            text: `Write a ‘C’ program to evaluate a given polynomial using function. (Use array).`,
             marks: 10,
           },
         ],
@@ -198,13 +199,12 @@ int main()
         questions: [
           {
             questionId: 1,
-            text: `Write a 'C' program to accept a string from a user and reverse it using Static
-            implementation of Stack.`,
+            text: `Write a ‘C’ program to accept a string from a user and reverse it using Static implementation of Stack.`,
             marks: 20,
           },
           {
             questionId: 2,
-            text: `Write a 'C' program to create a Circularly Doubly Linked list and display it.`,
+            text: `Write a ‘C’ program to create a Circularly Doubly Linked list and display it.`,
             marks: 10,
           },
         ],
@@ -214,14 +214,12 @@ int main()
         questions: [
           {
             questionId: 1,
-            text: `Write a program to create two singly linked lists of elements of type integer and
-            find the union of the linked lists. (Accept elements in the sorted order)`,
+            text: `Write a program to create two singly linked lists of elements of type integer and find the union of the linked lists. (Accept elements in the sorted order)`,
             marks: 20,
           },
           {
             questionId: 2,
-            text: `Write a 'C' program to read the adjacency matrix of a directed graph and convert
-            it into an adjacency list.`,
+            text: `Write a ‘C’ program to read the adjacency matrix of a directed graph and convert it into an adjacency list.`,
             marks: 10,
           },
         ],
@@ -231,12 +229,402 @@ int main()
         questions: [
           {
             questionId: 1,
-            text: "Write a menu driven program using ‘C’ for Binary Search Tree. The menu includes:\n- Create a Binary Search Tree\n- Traverse it by using Inorder and Postorder traversing technique",
+            text: `Write a menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Traverse it by using Inorder and Postorder traversing technique`,
             marks: 20,
           },
           {
             questionId: 2,
-            text: "Write a ‘C’ program to accept two polynomials and find the addition of accepted polynomials. (use array)",
+            text: `Write a ‘C’ program to accept two polynomials and find the addition of accepted polynomials. (Use array)`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 5,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Traverse it by using Inorder and Preorder traversing technique`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a linked list with a given number in which data part of each node contains an individual digit of the number.<br> (Ex. Suppose the number is 368 then the nodes of linked list should contain 3, 6, 8)`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 6,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Traverse it by using Preorder and Postorder traversing technique`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept and sort n elements in ascending order by using bubble sort.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 7,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Display<br> - Delete a given element from Binary Search Tree`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a singly linked list and count total number of nodes in it and display the list and total number of Nodes.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 8,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write menu driven program using ‘C’ for Binary Search Tree.<br> The menu includes:<br> - Create a Binary Search Tree<br> - Display<br> - Search the element in Binary Search Tree`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept and sort n elements in ascending order by using insertion sort.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 9,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a program to accept a postfix expression and evaluate the expression using the stack.<br> Example: Input: ab+cd-*<br> Values: a=4, b=2, c=5, d=3<br> Answer: 12`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a singly linked list, reverse it and display both the list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 10,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read ‘n’ integers and store them in a Binary search tree and display the nodes level wise.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to sort randomly generated array elements using Insertion sort method.<br> (Use Random Function)`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 11,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a menu driven program using ‘C’ for singly linked list:<br> - To create linked list.<br> - To display linked list<br> - To search node in linked list.<br> - Insert at last position`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a menu driven program using ‘C’ for Dynamic implementation of Queue for integers. The menu includes:<br> - Insert<br> - Delete<br> - Display<br> - Exit`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 12,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a C program that accepts the graph as an adjacency matrix and checks if the graph is undirected. The matrix for undirected graph is symmetric. Also calculate in-degree of all vertices:<br> - Read a graph as adjacency Matrix<br> - Check the matrix is symmetric or not<br> - Calculate indegree of all vertices`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept and sort n elements in ascending order using Selection sort method.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 13,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a C program to accept an infix expression and convert it into postfix form.<br> (Use Static Implementation of Stack)<br> Example: - A * B + C as AB*C+`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create doubly linked list and display nodes having odd value.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 14,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to accept a string from user and reverse it using Dynamic implementation of Stack.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept names from the user and sort in alphabetical order using bubble sort:<br> - Accept n name<br> - Bubble sort Function<br> - Display`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 15,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to accept an infix expression, convert it into its equivalent postfix expression and display the result.<br> (Use Dynamic Implementation of Stack)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write menu driven program using ‘C’ for Dynamic implementation of Stack. The menu includes following operations:<br> - Push<br> - Pop<br> - Display<br> - Exit`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 16,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program which accept the string and reverse each word of the string using Static implementation of stack.<br> Example: Input - This is an input string<br> Output - sihTsi na tupnignirts`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a Singly linked list. Accept the number from user, search the number in the list. If the number is present display the Position of node. If number not present print the message “Number not Found”.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 17,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read a postfix expression, evaluate it and display the result.<br> (Use Static Implementation of Stack).`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept the names of cities and store them in array. Accept the city name from user and use linear search algorithm to check whether the city is present in array or not.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 18,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read ‘n’ integers and store them in a binary Search tree structure and count the following and display it:<br> - Number of nodes<br> - Degree of tree<br> - Leaf nodes`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to accept and sort n elements in ascending order using Merge sort method.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 19,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program which accept the string and reverse each word of the string using Dynamic implementation of stack.<br> Example: Input - This is an input string<br> Output - sihTsi na tupnignirts`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a singly Link list and display its alternative nodes.<br> (start displaying from first node)`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 20,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program which accept the string and check whether the string is Palindrome or not using stack.<br> (Use Static/Dynamic implementation of Stack)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to swap mth and nth element of singly linked list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 21,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read an adjacency matrix of a directed graph and traverse using BFS.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program Accept n elements from user store it in an array. Accept a value from the user and use linear/Sequential search method to check whether the value is present in array or not. Display proper message.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 22,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program which accept an Expression and check whether the expression is Parenthesized or not using stack.<br> (Use Static/Dynamic implementation of Stack)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to count all non-zero elements, odd numbers and even numbers in the singly linked list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 23,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a menu driven program using ‘C’ for singly linked list:<br> - To create linked list.<br> - To display linked list<br> - To insert node at last position of linked list.<br> - To delete node from specific position of linked list.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create a random array of n integers. Accept a value x from user and use Binary search algorithm to check whether the number is present in array or not.<br> (Students can accept sorted array or can use any sorting method to sort the array)`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 24,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read a postfix expression, evaluate it and display the result.<br> (Use Dynamic Implementation of Stack)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to remove last node of the singly linked list and insert it at the beginning of list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 25,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to read an adjacency matrix of a directed graph and traverse it using DFS.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a menu driven program using ‘C’ for singly linked list:<br> - To create linked list.<br> - To display linked list`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 26,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to accept an infix expression, convert it into its equivalent prefix expression and display the result.<br> (Use Static Implementation of Stack)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create two singly linked lists and concatenate one list at the end of another list.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 27,
+        questions: [
+          {
+            questionId: 1,
+            text: `Implement Static implementation of circular queue of integers with following operation:<br> - Initialize()<br> - Insert()<br> - Delete()<br> - IsEmpty()<br> - IsFull()<br> - Display()`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create Doubly Link list and display it.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 28,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to create a Binary Search tree and implement following functions:<br> - Create Binary Search Tree<br> - Search a node in binary search tree<br> - Display a binary search tree (Post order Traversal)`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to read n integers and create two lists such that all positive numbers are in one list and negative numbers are in another list. Display both the lists.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 29,
+        questions: [
+          {
+            questionId: 1,
+            text: `Write a ‘C’ program to create a Binary tree, traverse it using recursive operations like inorder, preorder and postorder and display the result of each one separately.`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to create Circular Singly Link list and display it.`,
+            marks: 10,
+          },
+        ],
+      },
+      {
+        slipId: 30,
+        questions: [
+          {
+            questionId: 1,
+            text: `Implement Dynamic implementation of circular queue of integers with following operation:<br> - Initialize()<br> - Insert()<br> - Delete()<br> - IsEmpty()<br> - IsFull()<br> - Display()`,
+            marks: 20,
+          },
+          {
+            questionId: 2,
+            text: `Write a ‘C’ program to sort elements of a singly linked list in ascending order and display the sorted List.`,
             marks: 10,
           },
         ],
