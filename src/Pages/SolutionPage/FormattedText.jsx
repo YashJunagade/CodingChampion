bbdb16aad598bc16a37c79f18a20b928208e5fd8
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-26 02:44:18 +0530

    formated solution text

---
+++ src/Pages/SolutionPage/FormattedText.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FormattedText = ({ text }) => {
+  return (
+    <div className="formatted-text">
+      {text.split("\n").map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default FormattedText;
