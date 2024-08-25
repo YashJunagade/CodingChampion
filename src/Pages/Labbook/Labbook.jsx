8b2b2b02da4a87fdd6f4564aa51c6b402942d511
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-25 13:48:52 +0530

    labbook page added as future feature

--- src/Pages/Labbook/Labbook.jsx
+++ src/Pages/Labbook/Labbook.jsx
@@ -5,14 +5,49 @@ import "./Labbook.css";
 
 function Labbook() {
   return (
-    <section className="labbookPageSection">
-      <Navbar />
-      <div className="labbookMainContainer">
-        <SideBar />
-        <Menu />
-      </div>
-    </section>
+    // <section className="labbookPageSection">
+    //   <Navbar />
+    //   <div className="labbookMainContainer">
+    //     <SideBar />
+    //     <Menu />
+    //   </div>
+    // </section>
+    <div style={styles.container}>
+      <h1 style={styles.header}>🚧 Our team is working on this feature!</h1>
+      <p style={styles.message}>Please try again after some time.</p>
+      <a href="/" style={styles.link}>
+        Go Back To Home
+      </a>
+    </div>
   );
 }
 
 export default Labbook;
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "50px",
+    backgroundColor: "#f9f9f9",
+    borderRadius: "8px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+  },
+  header: {
+    fontSize: "40px",
+    color: "#333",
+    marginBottom: "20px",
+  },
+  message: {
+    fontSize: "20px",
+    color: "#666",
+  },
+  link: {
+    display: "inline-block",
+    marginTop: "20px",
+    padding: "10px 20px",
+    textDecoration: "none",
+    color: "#007bff",
+    borderRadius: "4px",
+    border: "1px solid #007bff",
+  },
+};
