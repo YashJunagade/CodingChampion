b352e2de7f6181bcd65fe96a8cc4620d4ab197d1
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-26 15:49:50 +0530

    added more security for api keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-T0KmjIYOnTVTWzM-wydvsU4zNzV352k",
-  authDomain: "unknown-bca.firebaseapp.com",
-  projectId: "unknown-bca",
-  storageBucket: "unknown-bca.appspot.com",
-  messagingSenderId: "755061603694",
-  appId: "1:755061603694:web:16e70653cc06b01e91157f",
-  measurementId: "G-60HLFV9FD4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const db=getFirestore(app);
+export const db = getFirestore(app);
 
 export default app;
