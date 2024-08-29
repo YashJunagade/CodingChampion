64997e628334fd49c57063ddf694b4356edb34f1
Devotion <letsmaildipak@gmail.com>
2024-08-29 20:34:57 +0530

    added tailwind for features container, for mobile screens md and xl remaining ,(minor bugs still remaining) added basic footer and changes in tailwind config

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,44 @@
 import Navbar from "./Navbar";
 import FeaturesContainer from "./FeaturesContainer";
+import Footer from "../../Components/Footer/Footer";
 
 function Home() {
-  return (
-    <>
-      <Navbar />
-      <div className="heroContainer">
-        <h1 className="text-xl font-bold text-red-500">
-          what we provide you in this platform happy to help you
-        </h1>
-        <div className="containers flex flex-col md:grid md:grid-cols-2">
-          <FeaturesContainer />
-          <FeaturesContainer />
-          <FeaturesContainer />
-          <FeaturesContainer />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="heroContainer">
+                <h1 className="text-xl font-bold text-red-500 text-center mt-4">
+                    <span className="text-3xl block">Gear Up for Success:</span>
+                    <span className="text-2xl">
+                        Your Ultimate Preparation Hub!
+                    </span>
+                </h1>
+                <div className="flex flex-col md:grid md:grid-cols-2">
+                    <FeaturesContainer
+                        featureTitle="Practical Slips"
+                        featureDescription="FY, SY and TY BBA(C.A.) practical slips with solutions"
+                        pageRoute="/slip"
+                    />
+                    <FeaturesContainer
+                        featureTitle="Lab Book"
+                        featureDescription="FY, SY and TY BBA(C.A.) Lab book Assignments with solutions"
+                        pageRoute="/slip"
+                    />
+                    <FeaturesContainer
+                        featureTitle="Data Structures & Algorithms"
+                        featureDescription="Practice your DSA skills with 1000+ selected questions"
+                        pageRoute="/dsa"
+                    />
+                    <FeaturesContainer
+                        featureTitle="Roadmap"
+                        featureDescription="Roadmap with Recommended courses and Youtube links"
+                        pageRoute="/roadmaps"
+                    />
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
--- src/Pages/Slip/Menu.jsx
+++ src/Pages/Slip/Menu.jsx
@@ -1,188 +1,78 @@
-import { useState } from "react";
 import "./Menu.css";
+
 import { Link } from "react-router-dom";
 
 function Menu() {
-  const [visibleSections, setVisibleSections] = useState({
-    firstYear: false,
-    semI: false,
-    semII: false,
-    secondYear: false,
-    semIII: false,
-    semIV: false,
-    thirdYear: false,
-    semV: false,
-    semVI: false,
-  });
-
-  // Toggle the visibility of sections
-  const toggleSection = (section) => {
-    setVisibleSections((prevState) => ({
-      ...prevState,
-      [section]: !prevState[section],
-    }));
-  };
-
   return (
-    <div className="slip-solution">
-      <div className="year" onClick={() => toggleSection("firstYear")}>
-        <div className="year-title">First Year Slip Solution</div>
-        {visibleSections.firstYear && (
-          <div className="semesters">
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semI");
-              }}
-            >
-              Sem - I
-              {visibleSections.semI && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/C Programming/slipList">C Programming </Link>
-                  </div>
-                  <div>
-                    <Link to="/DBMS/slipList">
-                      DataBase Management System (DBMS)
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semII");
-              }}
-            >
-              Sem - II
-              {visibleSections.semII && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/Web Technology/slipList">Web Technology </Link>
-                  </div>
-                  <Link to="/RDBMS/slipList">Relational DataBase (RDBMS) </Link>
-                </div>
-              )}
-            </div>
+    <>
+      <div className="flex flex-col w-full">
+        {/* first year subjects  */}
+        <div className="h-60 bg-primary w-full mt-4 p-6 flex justify-evenly">
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full md:h-full text-center">
+            <Link to="/C Programming/slipList">C Programming </Link>
           </div>
-        )}
-      </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/DBMS/slipList">DataBase Management System (DBMS)</Link>
+          </div>
 
-      <div className="year" onClick={() => toggleSection("secondYear")}>
-        <div className="year-title">Second Year Slip Solution</div>
-        {visibleSections.secondYear && (
-          <div className="semesters">
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semIII");
-              }}
-            >
-              Sem - III
-              {visibleSections.semIII && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/Data Structure/slipList">Data Structure</Link>
-                  </div>
-                  <div>
-                    <Link to="/Big Data/slipList">Big Data</Link>
-                  </div>
-                  <div>
-                    <Link to="/Php/slipList">Php</Link>
-                  </div>
-                  <div>
-                    <Link to="/Angular JS/slipList">AngularJS</Link>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semIV");
-              }}
-            >
-              Sem - IV
-              {visibleSections.semIV && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/CPP/slipList">CPP</Link>
-                  </div>
-                  <div>
-                    <Link to="/Advace Php/slipList">Advace Php</Link>
-                  </div>
-                  <div>
-                    <Link to="/Node JS/slipList">Node JS</Link>
-                  </div>
-                </div>
-              )}
-            </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Web Technology/slipList">Web Technology </Link>
           </div>
-        )}
-      </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/RDBMS/slipList">Relational DataBase (RDBMS) </Link>
+          </div>
+        </div>
 
-      <div className="year" onClick={() => toggleSection("thirdYear")}>
-        <div className="year-title">Third Year Slip Solution</div>
-        {visibleSections.thirdYear && (
-          <div className="semesters">
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semV");
-              }}
-            >
-              Sem - V
-              {visibleSections.semV && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/Core Java/slipList">Core Java</Link>
-                  </div>
-                  <div>
-                    <Link to="/Python/slipList">Python</Link>
-                  </div>
-                  <div>
-                    <Link to="/MongoDB/slipList">MongoDB</Link>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div
-              className="semester"
-              onClick={(e) => {
-                e.stopPropagation();
-                toggleSection("semVI");
-              }}
-            >
-              Sem - VI
-              {visibleSections.semVI && (
-                <div className="subjects">
-                  <div>
-                    <Link to="/Advance Java/slipList">Advance Java</Link>
-                  </div>
-                  <div>
-                    <Link to="/Android Programming/slipList">
-                      Android Programming
-                    </Link>
-                  </div>
-                  <div>
-                    <Link to="/Dot Net Framework/slipList">
-                      Dot Net Framework
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </div>
+        {/* second year subjects  */}
+        <div className="h-60 bg-primary w-full mt-4 p-6 flex justify-evenly">
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Data Structure/slipList">Data Structure</Link>
           </div>
-        )}
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Big Data/slipList">Big Data</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Php/slipList">Php</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Angular JS/slipList">AngularJS</Link>
+          </div>
+
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/CPP/slipList">CPP</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Advace Php/slipList">Advace Php</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Node JS/slipList">Node JS</Link>
+          </div>
+        </div>
+        {/* third year subjects  */}
+
+        <div className="h-60 bg-primary w-full mt-4 p-6 flex justify-evenly">
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Core Java/slipList">Core Java</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Python/slipList">Python</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/MongoDB/slipList">MongoDB</Link>
+          </div>
+
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Advance Java/slipList">Advance Java</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Android Programming/slipList">Android Programming</Link>
+          </div>
+          <div className=" flex flex-col bg-primary2 hadow-md  rounded w-30 md:w-40 h-full   md:h-full">
+            <Link to="/Dot Net Framework/slipList">Dot Net Framework</Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
 export default Menu;
