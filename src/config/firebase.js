6125a8d8e5d90a1a3bc11fe4e492fcba8bf4b50d
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-09-01 19:02:58 +0530

    config changes in firestore{auth}

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
@@ -12,13 +12,13 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
 // Export the authentication and Firestore instances
-export const auth = getAuth();
-export const db = getFirestore(app);
+export const auth = getAuth()
+export const db = getFirestore(app)
 
-export default app;
+export default app
