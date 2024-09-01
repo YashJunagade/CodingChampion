cc1d7779117bc34d8c127e524b7e43859e436385
Devotion <letsmaildipak@gmail.com>
2024-09-01 05:35:44 +0530

    minor changes in Subject for better look

--- src/Components/Subjects/Subject.jsx
+++ src/Components/Subjects/Subject.jsx
@@ -16,7 +16,7 @@ function Subject({ subRoute, subName }) {
     >
       <Link to={subRoute} className="mx-auto block">
         <motion.div
-          className="flex flex-col h-44 w-32 bg-white shadow-even-shadow  rounded-custom text-center sm:h-48 sm:w-36 lg:h-52 lg:w-40 mx-auto"
+          className="flex flex-col h-44 w-32 bg-primary shadow-even-shadow  rounded-custom text-center sm:h-48 sm:w-36 lg:h-52 lg:w-40 mx-auto"
           whileHover={{
             boxShadow: '0px 0px 8px rgba(255, 80, 1, 0.2)',
           }}
@@ -31,9 +31,14 @@ function Subject({ subRoute, subName }) {
             whileHover={{ rotate: 5 }}
             transition={{ duration: 0.2 }}
           />
-          <h3 className="font-bold text-accent  text-center h-[35%] p-1">
+          <motion.h3
+            className="font-bold text-primary rounded-b-custom text-center bg-black h-[35%] p-1 flex justify-center items-center"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             {subName}
-          </h3>
+          </motion.h3>
         </motion.div>
       </Link>
     </motion.div>
