dabe94f748442c71ab0d3a67bf282570e33fee89
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-09-09 00:13:14 +0530

    fixed the profile change modal for all screens

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -4,6 +4,8 @@ import { auth } from '../../config/firebase'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
 import Sidebar from '../../Components/SideBar/SideBar'
+import { FaRegEdit } from 'react-icons/fa'
+import { ImCross } from 'react-icons/im'
 
 function Profile() {
   const { userDetails, loading, isLoggedIn, updateProfilePic } = useUser()
@@ -47,7 +49,7 @@ function Profile() {
   }
 
   return (
-    <div className="md:ml-52 md:mt-16 lg:ml-60 md:flex dark:text-white text-black">
+    <div className="md:ml-52 md:mt-16 lg:ml-60 md:flex dark:text-white text-black h-screen">
       {/* Sidebar should have a fixed width */}
       {/* <Sidebar className="w-1/4 md:w-1/5"></Sidebar> */}
 
@@ -55,34 +57,43 @@ function Profile() {
       <div className="p-4 w-full md:w-4/5">
         {userDetails ? (
           <>
-            <p>Email: {userDetails.email}</p>
             <p>User Name: {userDetails.userName || 'No username set'}</p>
-            <img
-              src={`${selectedPic}?t=${new Date().getTime()}`}
-              alt="Profile"
-              className="w-40 h-40 cursor-pointer rounded-full my-5"
-              onClick={() => setModalOpen(true)}
-            />
+            <p>Email: {userDetails.email}</p>
+
+            {/* Container for Profile Pic and Edit Icon */}
+            <div className="relative w-40 h-40 my-5">
+              <FaRegEdit
+                className="absolute top-1 right-1 text-xl text-black dark:text-white cursor-pointer hover:text-accent"
+                onClick={() => setModalOpen(true)}
+              />
+              <img
+                src={`${selectedPic}?t=${new Date().getTime()}`}
+                alt="Profile"
+                className="w-full h-full cursor-pointer rounded-full"
+                onClick={() => setModalOpen(true)}
+              />
+            </div>
+
             <button
               onClick={handleSignOut}
-              className="mt-4 bg-accent text-white px-4 py-2 rounded"
+              className="mt-4 bg-accent text-white px-4 py-2 rounded mx-8"
             >
               Sign Out
             </button>
 
             {modalOpen && (
               <div className="fixed inset-0 text-black bg-black bg-opacity-50 flex justify-center items-center z-50">
-                <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-w-lg relative">
+                <div className="bg-white p-6 rounded-lg shadow-lg w-[90%] max-w-md relative">
                   <span
-                    className="absolute top-2 right-3 text-xl cursor-pointer"
+                    className="absolute top-4 right-4 text-xl"
                     onClick={() => setModalOpen(false)}
                   >
-                    &times;
+                    <ImCross className="text-sm md:text-md text-black cursor-pointer" />
                   </span>
-                  <h2 className="text-lg font-semibold mb-4 ">
+                  <h2 className="text-lg font-semibold mb-4">
                     Select Profile Picture
                   </h2>
-                  <div className="flex flex-wrap gap-4">
+                  <div className="flex flex-wrap gap-3 md:gap-4">
                     {[
                       'https://res.cloudinary.com/yashjunagade/image/upload/v1725552109/1_e5x60h.jpg',
                       'https://res.cloudinary.com/yashjunagade/image/upload/v1725552109/2_waioan.jpg',
@@ -101,7 +112,7 @@ function Profile() {
                         src={`${picName}`}
                         alt={picName}
                         onClick={() => handlePicSelect(picName)}
-                        className="w-16 h-16 md:w-28 md:h-28 cursor-pointer border-2 border-gray-300 rounded-[50%] hover:border-blue-500"
+                        className="w-20 h-20 md:w-28 md:h-28 cursor-pointer border-2 border-gray-300 rounded-[50%] hover:border-blue-500"
                       />
                     ))}
                   </div>
