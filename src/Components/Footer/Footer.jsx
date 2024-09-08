c252ecc49a363d79430acd74a872a5367f3a3a29
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-09-08 13:22:33 +0530

    changed footer added logo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,29 @@
+import { Link } from 'react-router-dom'
+
 function Footer() {
   return (
     <footer className="mt-4 py-4 bg-offWhite dark:bg-black dark:text-white">
       {/* <div className="text-center font-bold">
         Made with ❤️ by Yash and Dipak
       </div> */}
-      <div className="mx-auto w-full items-center justify-between px-4">
+      <div className="mx-auto w-full items-center justify-between px-4 text-center">
         <div className="inline-flex items-center">
-          <img src="" alt="Unknown logo" />
+          <Link to="/">
+            <img
+              className="hidden dark:block"
+              src="https://res.cloudinary.com/yashjunagade/image/upload/v1725779658/DarkModeLogo_if19sr.svg"
+              alt="Unknown logo"
+              width={200}
+            />
+            <img
+              className="dark:hidden"
+              src="https://res.cloudinary.com/yashjunagade/image/upload/v1725779657/LightModeLogo_b4g7mj.svg"
+              alt="Unknown logo"
+              width={200}
+            />
+          </Link>
         </div>
-        <div className="mt-4">
+        <div className="-mt-4">
           <p className="text-sm font-medium text-primary2">
             © 2024 Unknown. All rights reserved.
           </p>
