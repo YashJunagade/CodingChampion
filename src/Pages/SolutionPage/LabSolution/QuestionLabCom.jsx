f3601663524fe291ac19cf7bf46fc07aab67a955
Devotion <letsmaildipak@gmail.com>
2024-09-10 11:19:38 +0530

    made same changes to lab questions component as slip questions component

--- src/Pages/SolutionPage/LabSolution/QuestionLabCom.jsx
+++ src/Pages/SolutionPage/LabSolution/QuestionLabCom.jsx
@@ -1,4 +1,4 @@
-function QuestionCom({ assignmentNo, setName, questionNo, questionText }) {
+function QuestionLabCom({ assignmentNo, setName, questionNo, questionText }) {
   function findName(setNo) {
     let setname = ''
     if (setNo === 1) {
@@ -13,7 +13,10 @@ function QuestionCom({ assignmentNo, setName, questionNo, questionText }) {
   }
 
   return (
-    <div className="h-full overflow-y-auto p-4 bg-[#1e1e1e]">
+    <div
+      className="h-[100%] overflow-y-auto p-4 bg-moreWhite dark:bg-[#1e1e1e] border-[2px] border-accent rounded-[10px]"
+      style={{ color: 'black' }}
+    >
       <div className="text-lg font-semibold mb-2 text-accent">
         Assignment No: {assignmentNo || 'N/A'}
       </div>
@@ -21,7 +24,7 @@ function QuestionCom({ assignmentNo, setName, questionNo, questionText }) {
         Set: {findName(setName) || 'N/A'}
       </div>
       <div
-        className="text-base mb-4 text-primary"
+        className="text-black dark:text-white mb-4 text-primary"
         dangerouslySetInnerHTML={{
           __html:
             questionNo && questionText
@@ -33,4 +36,4 @@ function QuestionCom({ assignmentNo, setName, questionNo, questionText }) {
   )
 }
 
-export default QuestionCom
+export default QuestionLabCom
