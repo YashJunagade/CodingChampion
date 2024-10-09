daee5b51f696f4fb16f367cbf00660939d68fc31
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-10-09 20:15:37 +0530

    added title and description for Labbook page

--- src/Pages/Labbook/Labbook.jsx
+++ src/Pages/Labbook/Labbook.jsx
@@ -4,17 +4,30 @@ import Menu from '../../Components/Subjects/Menu'
 
 function Labbook() {
   return (
-    <section className="">
-      {/* <Navbar></Navbar> */}
-      <div className="flex">
-        {/* <div className="hidden md:inline relative"> */}
-        {/* <SideBar></SideBar> */}
-        {/* </div> */}
-        <div className="w-full">
-          <Menu></Menu>
+    <>
+      <Helmet>
+        <title>
+          BBACA BCA Slip Labbook Solution First Year Second Year Third Year
+          BBACA BCA
+        </title>
+        <meta
+          name="description"
+          content="BBACA BCA Practical Slip Solution Labbook Solution First Year Second Year Third
+              Year BBACA BCA SPPU University Coding Champion C DBMS RDBMS Web Technology data structure Big Data Php Cpp Angular JS Advance Php Node JS Core Java Python MongoDB Advance Java Android Programming Dot Net Framework"
+        />
+      </Helmet>
+      <section className="">
+        {/* <Navbar></Navbar> */}
+        <div className="flex">
+          {/* <div className="hidden md:inline relative"> */}
+          {/* <SideBar></SideBar> */}
+          {/* </div> */}
+          <div className="w-full">
+            <Menu></Menu>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
