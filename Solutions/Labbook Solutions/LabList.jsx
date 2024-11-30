168d796c104faf95542703cb728b327830264eb3
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-11-30 20:21:01 +0530

    added code of adsense for lablist

--- Solutions/Labbook Solutions/LabList.jsx
+++ Solutions/Labbook Solutions/LabList.jsx
@@ -114,12 +114,20 @@ const LabList = () => {
               Sort {sortOrder === 'asc' ? 'Descending' : 'Ascending'}
             </motion.button>
           </div>
-          <GoogleAds
-            adSlot="7884444663"
-            uniqueKey="lablist-attop"
-            baseWidth={1000}
-            baseHeight={300}
-          />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1487916517080617"
+            crossorigin="anonymous"
+          ></script>
+          <ins
+            class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-1487916517080617"
+            data-ad-slot="7884444663"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
