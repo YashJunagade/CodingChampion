4524ae7876d05e96c22b4f4ab718a49b21c57c74
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-11-30 17:31:10 +0530

    fixed adsense

--- src/Pages/Labbook/Labbook.jsx
+++ src/Pages/Labbook/Labbook.jsx
@@ -2,6 +2,7 @@ import { Helmet } from 'react-helmet'
 import Navbar from '../../Components/Navbar/Navbar'
 import SideBar from '../../Components/SideBar/SideBar'
 import Menu from '../../Components/Subjects/Menu'
+import GoogleAdSense from '../../GoogleAdSense'
 
 function Labbook() {
   return (
