84e02599a0dc3020ec099ab0f384d4b0b170f49c
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-11-30 19:40:16 +0530

    testing manual ads on slip and labbook

--- src/Pages/Labbook/Labbook.jsx
+++ src/Pages/Labbook/Labbook.jsx
@@ -2,6 +2,7 @@ import { Helmet } from 'react-helmet'
 import Navbar from '../../Components/Navbar/Navbar'
 import SideBar from '../../Components/SideBar/SideBar'
 import Menu from '../../Components/Subjects/Menu'
+import GoogleAds from '../../GoogleAds'
 
 function Labbook() {
   return (
@@ -16,6 +17,11 @@ function Labbook() {
           content="BBACA BCA bbaca bba ca bca Practical Slip Solution Labbook Solution First Year Second Year Third
               Year BBACA BCA SPPU University Coding Champion C DBMS RDBMS Web Technology data structure Big Data Php Cpp Angular JS Advance Php Node JS Core Java Python MongoDB Advance Java Android Programming Dot Net Framework"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1487916517080617"
+          crossorigin="anonymous"
+        ></script>
       </Helmet>
       <section className="">
         {/* <Navbar></Navbar> */}
@@ -25,6 +31,12 @@ function Labbook() {
           {/* </div> */}
           <div className="w-full">
             <Menu></Menu>
+            <GoogleAds
+              adSlot="3195881308"
+              uniqueKey="labbok-atbottom"
+              baseWidth={800}
+              baseHeight={300}
+            />
           </div>
         </div>
       </section>
